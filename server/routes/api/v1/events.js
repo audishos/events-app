c56b2aa6c8d8b4beff868cea2998d4c76a811d5f
audishos <audisho.sada@gmail.com>
2017-11-13 23:39:28 -0500

    Adds mongoose. Starts implementing model

--- server/routes/api/v1/events.js
+++ server/routes/api/v1/events.js
@@ -2,7 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
 });
 
 module.exports = router;
